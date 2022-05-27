4d8646cb36cc5aac70e48ec8dec31d325fea0460
Ariel Rojas <arirojas50@gmail.com>
2022-05-27 16:10:53 -0300

    esqueleto game1

--- js/game1.js
+++ js/game1.js
@@ -1,20 +1,67 @@
-const grid = document.querySelector('.grid')
-let columnas=7
-let filas=2
-let squares = []
+const grid = document.querySelector('.grid');
+let shooterIndex = 202;
+let squares = [];
+let width = 15;
+let direction = 1;
+let invadersId;
+
+
 function createBoard(){
-    for(let i=0; i< columnas*filas; i++){
+    for(let i=0; i<225; i++){
         const square = document.createElement('div')
-        square.setAttribute('id',i)
-        square.innerHTML = 0
+        // square.setAttribute('id',i)
+        // square.innerHTML = 0
         grid.appendChild(square)
         squares.push(square)
     }
 }
+function range(start, end) {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+  }
+
+const alienInvaders = range(0, 9).concat(range(15, 24), range(30, 39))
+
 createBoard()
-console.log(squares)
+function draw(){
+    for(let i=0; i<alienInvaders.length;i++){
+        squares[alienInvaders[i]].classList.add('invader')
+    }
+}
+function remove(){
+    for(let i=0; i<alienInvaders.length;i++){
+        squares[alienInvaders[i]].classList.remove('invader')
+    }
+}
+draw()
+
+squares[shooterIndex].classList.add('shooter')
+// muevo shooters
 
 
-// math is not defined at (revisar error)
-let a = math.zeros(2, 2) 
-alert(a)
+function moveShooter(e){
+    squares[shooterIndex].classList.remove('shooter');
+    switch(e.key){
+        case 'ArrowLeft':
+            if (shooterIndex % width !== 0){shooterIndex -=1; break;}
+            else{break;}
+        case 'ArrowRight':
+            if (shooterIndex % width < width - 1){shooterIndex +=1; break;} 
+            else{break;}       
+
+    }
+    squares[shooterIndex].classList.add('shooter');
+}
+document.addEventListener('keydown', moveShooter)
+
+// muevo aliens
+function moveInvaders(){
+    const leftEdge = alienInvaders[0] % width === 0;
+    const rightEdge = alienInvaders[alienInvaders.length] % width === width  - 1;
+    remove()
+    for(let i=0; i<alienInvaders.length;i++){
+        alienInvaders[i] += direction;
+    }
+    draw();
+}
+
+invadersId = setInterval(moveInvaders,200)
